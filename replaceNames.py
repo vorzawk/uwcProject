52693294357a0b5a2fc0eea7fab824311876365b
vorzawk <21965736+vorzawk@users.noreply.github.com>
2019-07-24 14:44:02 -0500

    added patch.txt to avoid typos from being labeled as names, updated commonWords.txt, used regex to elegantly extract capitalized words rather than detecting and removing punctuation, also implemented check for synonyms of the word

--- replaceNames.py
+++ replaceNames.py
@@ -2,11 +2,17 @@ import xlrd
 import string
 import re
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet as wn
 
-h = set()
+patchWords = set()
+with open('patch.txt', 'r') as f:
+    for line in f:
+        patchWords.add(line.rstrip())
+
+commonWords = set()
 with open('commonWords.txt', 'r') as f:
     for line in f:
-        h.add(line.rstrip())
+        commonWords.add(line.rstrip())
 
 #workbook = xlrd.open_workbook('fakeSessionNotes_dummyFile.xlsx')
 workbook = xlrd.open_workbook('AllData_sample.xlsx')
@@ -18,44 +24,47 @@ namesList = []
 # Bad code, hardcoding the column in which the data is present
 # But this will do for now!
 notes = worksheet.col(0, start_rowx=0)
-# if I only use the split function to identify words, "~Katie", "-Daniel", "deal," etc. will be considered as words. In this case, some
-# names will be missed and some will get wrongly labeled as names.
-# The solution implemented here is to ignore all punctuation and use the first unbroken chunk of letters as the word. This causes some of
-# the punctuation to get missed.
-# Set up the translation table to eplace all of the punctuation with spaces.
-replace_punctuation = str.maketrans(string.punctuation, ' '*len(string.punctuation))
 lmtr = WordNetLemmatizer()
 for note in notes:
-    print(note)
+#    print('Original Note:\n', note.value)
+    print(note.value)
     wordsInNote = note.value.split()
 #    print(wordsInNote)
     namesRemoved = []
     for word in wordsInNote:
-#        print(word)
-        if any(c.isalpha() for c in word):
-            # stem the word by stripping all of its punctuation.(would it be a good idea to replace this with something else?"
-            tokens = word.translate(replace_punctuation).split()
-            stemWord = max(tokens, key=len)
-            # if the word ends with "ed" then check if the first part is present in the word list. This is to avoid words like Worked and 
-            # Checked from being labeled as names.
-            #m = re.search(r'(\w+)ed', stemWord)
-            #if m:
-            #    # group(0) returns the entire match while group(1) returns the first subgroup.
-            #    stemWord = m.group(1)
-            # The lemmatizer based on WordNet is very robust in reducing the various verb forms into their roots.
-            lmtWord = lmtr.lemmatize(stemWord.lower(), pos='v')
-            if lmtWord != stemWord.lower():
-                stemWord = lmtWord
-            # Ignores common nouns which occur as plurals. The singular form is likely included in the list of common words.
-            lmtWord = lmtr.lemmatize(stemWord.lower(), pos='n')
-            if lmtWord != stemWord.lower():
-                stemWord = lmtWord
-        else:
-            stemWord = ''
-#        print('stem', stemWord)
         # Only consider the words in which only the first letter is capitalized while checking the list of common words. This is because
         # these are the only canditates for being names.
-        if len(stemWord) > 1 and stemWord[0].isupper() and stemWord[1].islower() and stemWord.lower() not in h:
+        m = re.search(r'[A-Z][a-z]+', word)
+        if m:
+        # group(0) returns the entire match.
+            stemWord = m.group(0)
+        else:
+            stemWord = ''
+#        print('word', word)
+#        print('stem', stemWord)
+        synonyms = []
+#        antonyms = []
+        for syn in wn.synsets(stemWord):
+            for l in syn.lemmas():
+                synonyms.append(l.name())
+#                if l.antonyms():
+#                    antonyms.append(l.antonyms()[0].name())
+
+#       print(set(synonyms))
+#	print(set(antonyms))
+        if (
+            stemWord and
+            stemWord.lower() not in commonWords and
+            stemWord.lower() not in patchWords and
+            not set(synonyms) & commonWords and
+#            not set(antonyms) & h and
+            # Removed 'a' coz of Tonal!
+            (not wn.synsets(stemWord) or wn.synsets(stemWord)[0].pos() not in ('r','v')) and
+#            (not wn.synsets(stemWord) or all([synset.pos() not in ('a','r','v') for synset in wn.synsets(stemWord)])) and
+#            lmtr.lemmatize(stemWord.lower(), pos='v') == stemWord.lower() and
+            # Ignores common nouns which occur as plurals. The singular form is likely included in the list of common words.
+            len(lmtr.lemmatize(stemWord.lower(), pos='n')) == len(stemWord)
+        ):
             # Suggestion: compare stemWord with word, and append the punctuation to the [NAME] token.
             namesRemoved.append('[NAME]')
             namesList.append(stemWord)
@@ -64,12 +73,10 @@ for note in notes:
     # namesRemoved is the list of tokens with the names redacted. This statement creates sentences from them by including spaces between
     # them.
     outputNote = ' '.join(namesRemoved)
+#    print('Redacted Note: \n', outputNote)
     print(outputNote)
     print()
 print('total number of words labeled as names : ', len(namesList))
 print(namesList)
-print()
-from collections import Counter
-print(Counter(namesList))
 
 
